ac2b6280b7ef9a0acd0ada70107be0b059b14b31
htphongx4 <phonght@smartosc.com>
2023-03-28 10:09:47 +0700

    feat: updated is prod

--- hooks/useEnv.ts
+++ hooks/useEnv.ts
@@ -4,9 +4,9 @@ export const useEnv = () => {
 	const { WEBSITE_URL, NEXTAUTH_URL } = getEnvVariables()
 
 	return {
-		isProd: WEBSITE_URL === 'https://heros.xyz',
-		isDev: WEBSITE_URL === 'https://dev.heros.xyz',
-		isUat: WEBSITE_URL === 'https://uat.heros.xyz',
+		isProd: WEBSITE_URL === 'https://heros.xyz' || WEBSITE_URL === "https://www.heros.xyz",
+		isDev: WEBSITE_URL === 'https://dev.heros.xyz' || WEBSITE_URL === 'https://www.dev.heros.xyz',
+		isUat: WEBSITE_URL === 'https://uat.heros.xyz' || WEBSITE_URL === 'https://www.uat.heros.xyz',
 		isDevelopment: NEXTAUTH_URL?.includes('localhost'),
 	};
 };
