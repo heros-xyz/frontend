6ff488c410071d925357ea992ea1b10d0eb8a55e
Nacho Althabe <nacho.althabe@gmail.com>
2023-05-16 15:43:59 -0300

    Update

--- libs/dtl/user.ts
+++ libs/dtl/user.ts
@@ -18,6 +18,7 @@ export interface User {
   nationality: Nationality
   isFinishSetupAccount?: boolean
   profileType: "FAN" | "ATHLETE"
+  stripeCustomer?: string
 }
 
 export function useUploadAvatarToUser() {
